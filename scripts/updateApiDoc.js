31d8ed2e7caa0ed34d145bd2ee99ed14e04d48a7
Denis Masinovic <denis.masinovic@gmail.com>
2020-03-28 17:18:55 +0100

    Added scripts to automatically generate api docs

---
+++ scripts/updateApiDoc.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const fs = require('fs');
+
+const apiDocPrefix = 'api/';
+const projectName = path.basename(process.cwd());
+const sidebarLocation = path.resolve(__dirname, '../website/website/sidebars.js');
+const resultLocation = path.resolve(__dirname, `../website/api_sidebars/${projectName}.json`);
+
+console.log('Updating API doc sidebar file');
+if (!fs.existsSync(sidebarLocation)) {
+  console.error(`Could not find sidebar file in ${sidebarLocation}`);
+  process.exit(1);
+}
+
+const sidebarContent = require(sidebarLocation);
+const mappedDocumentationEntries = Object.entries(sidebarContent.docs).map(
+  ([categoryName, categoryEntries]) => ({
+    type: 'category',
+    label: categoryName,
+    items: categoryEntries.map((entry) => `${apiDocPrefix}${projectName}/${entry}`),
+  })
+);
+
+if (fs.existsSync(resultLocation)) {
+  console.log('Removing existing generated sidebar file');
+  fs.unlinkSync(resultLocation);
+}
+fs.writeFileSync(resultLocation, JSON.stringify(mappedDocumentationEntries));
+
+console.log('Removing parsed sidebar file');
+fs.unlinkSync(sidebarLocation);
+
+console.log('All done');
